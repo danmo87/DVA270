ac57c36e267aaeaf8c500a2f3752335a24079313
Maverick Irvine Nilsson <mavericknilsson@hotmail.com>
2024-05-22 11:17:12 +0200

    Added grupp_21 function and header

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <Daniel_GPIO.h> //Ni behöver alltså inte inkludera er egna UARTE write, förutsätt att det finns en uarte_write, uarte_init osv osv
 #include "grupp1.h" //inkludera er header fil.
+#include "grupp_21.h"
 
 /* By Daniel Morberg*/
 
@@ -18,7 +19,10 @@ int keypress = get_key();
   switch(keypress){
     case 1:
     //anrop till grupp 1's funktion
-    breaK;
+    break;
+
+    case 21:
+    start_game_grupp21();
   //osv osv
   }
 }
