73c3e7cbc86e55be7638d1b57b9db3b449142ae5
Andrea Lamprecht <alt23003@student.mdu.se>
2024-05-22 11:31:31 +0200

    Grupp3 har nu lagt till sitt spel

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # DVA270
 
 Ni kan lämna ett meddelande här om ni vill när ni ändå är inne och editerar, men ingenting osmakligt. /Daniel
+
+Grupp 3
+Målet med spelet är att samla alla Z, spelet avslutas när man når rutan längst ner i vänster hörn.
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <Daniel_GPIO.h> //Ni behöver alltså inte inkludera er egna UARTE write, förutsätt att det finns en uarte_write, uarte_init osv osv
 #include "grupp1.h" //inkludera er header fil.
+#include "Grupp3.h"
 #include "header17.h"//Grupp 17;Spelet går ut på att komma till rätt okänd koordinat mha. förflyttning med j,k,l,i
 
 
@@ -20,6 +21,9 @@ int keypress = get_key();
   switch(keypress){
     case 1:
     //anrop till grupp 1's funktion
+    break;
+    case 3:
+    start_game_grupp3();
     break;
     case 17:
     start_game_grupp17();
